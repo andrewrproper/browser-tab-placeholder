e68ec093660bbc786ae16efcc11af6e16e0a9876
Andrew Proper <andrewrproper@users.noreply.github.com>
2024-01-12 19:50:13 -0500

    refactor and enable both change and keyup events

--- script.js
+++ script.js
@@ -1,63 +1,35 @@
 
-// This max is based on firefox mobile, with tabs in list display format.
-const maxTitleWithDelimLength = 33; 
-const endDelimPart = ' .';
 
-// This factor is needed because title display characters are wider than just dots.
-const endDelimPartFactor = 1.7; 
-
-const titlePrefix = "|| ";
-const titleSuffix = " ||";
-
-setTitleTo("..."); // default
-
-// https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
-const params = new Proxy(
-  new URLSearchParams(window.location.search), {
-    get: (searchParams, prop) => searchParams.get(prop)
-  }
-);
-
-function setTitleTo (title) {
-  const titleDisplayLength = title.length + titlePrefix.length + titleSuffix.length;
+function setTitleTo (title, conf) {
+  const titleDisplayLength = title.length + conf.titlePrefix.length + conf.titleSuffix.length;
 
   let endDelim = "";
-  const lenDiff = maxTitleWithDelimLength - titleDisplayLength;
+  const lenDiff = conf.maxTitleWithDelimLength - titleDisplayLength;
   if ( lenDiff > 0 ) {
-    const repeatNum = Math.floor(lenDiff / endDelimPart.length * endDelimPartFactor);
+    const repeatNum = Math.floor(lenDiff / conf.endDelimPart.length * conf.endDelimPartFactor);
     /*
     console.debug("length calc", {
-      maxTitleWithDelimLength: maxTitleWithDelimLength,
+      conf.maxTitleWithDelimLength: maxTitleWithDelimLength,
       titleDisplayLength: titleDisplayLength, 
       lenDiff: lenDiff, 
       repeatNum: repeatNum
     });
     */
-    if ( repeatNum > 0 ) endDelim = endDelimPart.repeat(repeatNum);
+    if ( repeatNum > 0 ) endDelim = conf.endDelimPart.repeat(repeatNum);
   }
 
   document.getElementById("title").innerHTML = title;
 
-  const newDocTitle = titlePrefix + title + titleSuffix + " " + endDelim;
+  const newDocTitle = conf.titlePrefix + title + conf.titleSuffix + " " + endDelim;
   document.title = newDocTitle;
 }
-let input = document.getElementById("set-title");
-  
-console.debug(params);
-let title = params.t;
-if ( title !== undefined && title !== null && title !== "" ) {
-  //console.debug("found title from params: "+title);
-  setTitleTo(title);
-  input.value = title;
-} else {
-  console.debug("no title found from params");
-}
 
-input.addEventListener('keyup', function(event) {
-  let title = this.value;
+
+function inputChanged (input, conf) {
+  let title = input.value;
   if ( title !== undefined && title !== null) {
     //console.debug("found title from text input: "+title);
-    setTitleTo(title || "...");
+    setTitleTo(title || "...", conf);
 
 
     let searchParams = new URLSearchParams(window.location.search);
@@ -69,21 +41,70 @@ input.addEventListener('keyup', function(event) {
 
     window.history.pushState({}, "", newURL);
   } else {
-    console.debug("no title found from text input");
+    console.debug("no defined non-null title found from text input");
   }
-});
-
-input.focus();
+}
 
 
-let aboutButton = document.getElementById("about-button");
-let aboutHTML = document.getElementById("about-html");
+function initInput (conf) {
+  // https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+  const params = new Proxy(
+    new URLSearchParams(window.location.search), {
+      get: (searchParams, prop) => searchParams.get(prop)
+    }
+  );
 
-aboutButton.addEventListener('click', function(event) {
-  // toggle between displayed and not displayed
-  if ( aboutHTML.style.display == 'none' ) {
-    aboutHTML.style.display = '';
-  } else {
-    aboutHTML.style.display = 'none';
+  let input = document.getElementById("set-title");
+    
+  let title = params.t;
+  if ( title !== undefined && title !== null && title !== "" ) {
+    setTitleTo(title, conf); // default
+    input.value = title;
   }
-});
+
+  input.addEventListener('keyup',  function() { inputChanged(input, conf); });
+  input.addEventListener('change', function() { inputChanged(input, conf); });
+
+  input.focus();
+}
+
+
+function initAbout() {
+
+  let aboutButton = document.getElementById("about-button");
+  let aboutHTML = document.getElementById("about-html");
+
+  aboutButton.addEventListener('click', function(event) {
+    // toggle between displayed and not displayed
+    if ( aboutHTML.style.display == 'none' ) {
+      aboutHTML.style.display = '';
+    } else {
+      aboutHTML.style.display = 'none';
+    }
+  });
+
+}
+
+
+function init () {
+  const conf = {
+    maxTitleWithDelimLength: 33, 
+    endDelimPart: ' .',
+
+    // This factor is needed because title display characters are wider than just dots.
+    endDelimPartFactor: 1.7, 
+
+    titlePrefix: "|| ",
+    titleSuffix: " ||",
+  };
+
+  setTitleTo("...", conf); // default
+
+  initInput(conf);
+
+  initAbout(conf);
+}
+
+
+init();
+
